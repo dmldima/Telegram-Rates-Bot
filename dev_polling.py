8f6b87e8ec4e0e53e69f2f4faca50ac4bb2a7285
dmldima <dmldima@gmail.com>
2025-10-25 17:28:06 +0200

    Create dev_polling.py

---
+++ dev_polling.py
@@ -0,0 +1,13 @@
+import asyncio
+import os
+from aiogram import Bot, Dispatcher
+from handlers import router
+
+async def main():
+    bot = Bot(token=os.getenv("BOT_TOKEN"))
+    dp = Dispatcher()
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
