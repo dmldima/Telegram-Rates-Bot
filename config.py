38df3441149b3f43145c698a874d86e83bbb0e98
dmldima <dmldima@gmail.com>
2025-10-25 17:24:19 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+
+
+```python
+SUPPORTED_PAIRS = {
+    "EUR/USD", "EUR/GBP", "EUR/CHF", "USD/EUR", "USD/GBP", "USD/CHF", "EUR/SGD", "USD/SGD",
+    "UAH/EUR", "UAH/GBP", "UAH/USD",
+}
+
+CURRENCY_ALIASES = {
+    "GPB": "GBP",
+    "gpb": "GBP",
+}
+```
